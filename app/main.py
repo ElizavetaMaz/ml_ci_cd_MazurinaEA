3edd88e3f6c5563a59c98d324238c67ac8cd3f90
ElizavetaMaz <ladymazurina1@gmail.com>
2025-12-14 18:35:52 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 import os
 import joblib
+import logging
+from prometheus_client import Counter
+
+# Настройка логов
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+logger = logging.getLogger(__name__)
+
+requests_total = Counter("requests_total", "Total number of requests", ["endpoint"])
 
 app = FastAPI()
 VERSION = os.getenv("MODEL_VERSION", "v1.0.0")
@@ -17,15 +24,21 @@ class PredictRequest(BaseModel):
 
 @app.get("/health")
 def health():
+    requests_total.labels(endpoint="/health").inc()
+    logger.info("Зашли в Health")
     return {"status": "ok", "version": VERSION}
 
 @app.post("/predict")
 def predict(request: PredictRequest):
+    requests_total.labels(endpoint="/predict").inc()
+    logger.info(f"Начинаю предикт для интпута: {request.x}")
     X = np.array(request.x).reshape(1, -1)
     y_pred = model.predict(X)[0]
+    logger.info(f"Получен предикт: {y_pred}")
 
     return {
         "status": "ok",
         "prediction": int(y_pred),
         "version": VERSION
     }
+
